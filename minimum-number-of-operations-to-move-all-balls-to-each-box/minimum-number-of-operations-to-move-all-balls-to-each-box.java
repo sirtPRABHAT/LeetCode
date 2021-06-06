c37b7cc04771bd9aebf41e8ba9dd14fb8dda8a37
Prabhat Kumar Safi <prabhat2000safi@gmail.com>
2021-06-06 19:26:40 +0530

    Update minimum-number-of-operations-to-move-all-balls-to-each-box.java

--- minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[] minOperations(String box) {
-       int boxes[] = new int[box.length()];
-        for(int i = 0; i < box.length(); i++) {
-            boxes[i] = Integer.parseInt(String.valueOf(box.charAt(i)));
-            //System.out.println(box.charAt(i));
-        }
         List<Integer> scan = new ArrayList<Integer>();
-        for(int i = 0; i < boxes.length; i++){
-            if(boxes[i] ==1)
+        for(int i = 0; i < box.length(); i++) {
+            if(box.charAt(i) == '1')
                 scan.add(i);
         }
-        int result[] = new int[boxes.length];
-        for(int i = 0; i < boxes.length; i ++){
+        
+        int result[] = new int[box.length()];
+        for(int i = 0; i < box.length(); i++){
             int d = 0;
             for(int j = 0; j < scan.size(); j ++){
                  d += Math.abs(scan.get(j)-i);
