6fbfb6e3b339b6116cf1a76b71d75fc011387842
Prabhat Kumar Safi <prabhat2000safi@gmail.com>
2021-06-10 11:37:17 +0530

    Time: 5 ms (5.70%), Space: 38.8 MB (74.88%) - LeetHub

---
+++ kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,8 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = Arrays.stream(candies).max().getAsInt();
+        return Arrays.stream(candies).mapToObj(candy -> candy + extraCandies >= max).collect(Collectors.toList());
+    }
+
+//https://www.geeksforgeeks.org/intstream-maptoobj-java/
+}
